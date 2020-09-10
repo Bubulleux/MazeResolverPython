eb6bbe8668ccbc0dc408564c125fd2990693ec85
Bubulle <bubulle-lol@yahoo.fr>
2020-09-10 22:40:15 +0200

    maze classe

--- MazeResolverPython/MazeResolverPython.py
+++ MazeResolverPython/MazeResolverPython.py
@@ -4,92 +4,6 @@ import sys
 import colorsys
 import time
 
-class cell:
-    whall = False
-    groupCellsIndex = 2
-    distAtStart = -1
-
-class Maze:
-    map = []
-    def mazeIsComplex():
-        for row in map:
-            for col in row:
-                if not col.whall and col.groupCellsIndex !=  map[1][1].groupCellsIndex:
-                    return False
-        return True
-
-    def findDestructibleWhall():
-        while True:
-            rnd = random.Random()
-            x = rnd.randint(1, len(map) - 2)
-            y = 0
-            if x % 2 == 0:
-                y = rnd.randint(0, ((len(map) - 1) / 2) -1) * 2 +1
-            else:
-                y = rnd.randint(0, ((len(map) - 1) / 2) -2) * 2 +2
-            if map[x][y].whall ==  True:
-                break
-        return [x, y];
-
-    def generateMaze(size):
-        map = [[cell() for col in range(cols)] for row in range(rows)]
-        if size % 2 == 0:
-            size  += 1
-        i = 0
-        for row in range(size):
-            for col in range(size):
-                is_whall = (row % 2 == 0) or (col % 2 == 0) or row == 0 or row == size-1 or col == 0 or col == size-1
-                map[row][col].whall = is_whall
-                if not is_whall:
-                    map[row][col].groupCellsIndex = i
-                    i += 1
-        map = makeComplexMaze(map ,size)
-    
-    def makeComplexMaze(size):
-        while not mazeIsComplex(map):
-            rnd = random.Random()
-            cell_1 = []
-            cell_2 = []
-            whall = findDestructibleWhall(map)
-            if whall[0] % 2 == 0:
-                cell_1 = [whall[0] - 1 , whall[1]]
-                cell_2 = [whall[0] + 1 , whall[1]]
-            else:
-                cell_1 = [whall[0], whall[1] - 1]
-                cell_2 = [whall[0], whall[1] + 1]
-            print(f"{cell_1[0]},{cell_1[1]}  {map[cell_1[0]][cell_1[1]].groupCellsIndex}  {whall[0]} {whall[1]}")
-            if map[cell_1[0]][cell_1[1]].groupCellsIndex != map[cell_2[0]][cell_2[1]].groupCellsIndex:
-                map[whall[0]][whall[1]].whall = False
-                map[whall[0]][whall[1]].groupCellsIndex = map[cell_1[0]][cell_1[1]].groupCellsIndex
-                valueDel = map[cell_2[0]][cell_2[1]].groupCellsIndex
-                for row in range(size):
-                    for col in range(size):
-                        if map[row][col].groupCellsIndex == valueDel:
-                            map[row][col].groupCellsIndex = map[cell_1[0]][cell_1[1]].groupCellsIndex 
-        for x in range(100):
-            whall = findDestructibleWhall(map)
-            map[whall[0]][whall[1]].whall = False
-        map[1][0].whall = False
-        map[size-2][size-1].whall = False
-        map[size-2][size-1].distAtStart = 0
-
-    def findWay():
-        futurMap = map
-        for row in range(len(map)):
-            for col in range(len(map)):
-                if not map[row][col].whall and map[row][col].distAtStart == -1:
-                    posToCheck = [[0,1],[0,-1],[1,0],[-1,0]]
-                    if row == 1 and col == 0:
-                        posToCheck = [[0,1]]
-                    for pos in posToCheck:
-                        if map[row + pos[0]][ col + pos[1]].distAtStart != -1:
-                            futurMap[row][col].distAtStart = map[row + pos[0]][ col + pos[1]].distAtStart + 1
-                            break
-        map = futurMap
-
-
-
-
 def hsv_to_rgb2(h, s, v):
         if s == 0.0: v*=255; return (v, v, v)
         i = int(h*6.) # XXX assume int() truncates!
@@ -105,9 +19,91 @@ def hsvToHex(h,s,v):
     return "#%02x%02x%02x" %  hsv_to_rgb2(h,s,v)
 
 
+class cell:
+    whall = False
+    groupCellsIndex = 2
+    distAtStart = -1
 
+class Maze:
+    map = []
+    row = 0
+    col = 0
+    breakabel_whall = []
+    def mazeIsComplex(self):
+        for row in self.map:
+            for col in row:
+                if not col.whall and col.groupCellsIndex !=  self.map[1][1].groupCellsIndex:
+                    return False
+        return True
 
-        
+    def findDestructibleWhall(self):
+        print(len(self.breakabel_whall))
+        rnd = random.Random()
+        index = rnd.randint(0, len(self.breakabel_whall) - 1)
+        pos = self.breakabel_whall[index]
+        del self.breakabel_whall[index]
+        return pos;
+
+    def makeComplexMaze(self):
+        while not self.mazeIsComplex():
+            rnd = random.Random()
+            cell_1 = []
+            cell_2 = []
+            whall = self.findDestructibleWhall()
+            if whall[0] % 2 == 0:
+                cell_1 = [whall[0] - 1 , whall[1]]
+                cell_2 = [whall[0] + 1 , whall[1]]
+            else:
+                cell_1 = [whall[0], whall[1] - 1]
+                cell_2 = [whall[0], whall[1] + 1]
+            print(f"{cell_1[0]},{cell_1[1]}  {self.map[cell_1[0]][cell_1[1]].groupCellsIndex}  {whall[0]} {whall[1]}")
+            if self.map[cell_1[0]][cell_1[1]].groupCellsIndex != self.map[cell_2[0]][cell_2[1]].groupCellsIndex:
+                self.map[whall[0]][whall[1]].whall = False
+                self.map[whall[0]][whall[1]].groupCellsIndex = self.map[cell_1[0]][cell_1[1]].groupCellsIndex
+                valueDel = self.map[cell_2[0]][cell_2[1]].groupCellsIndex
+                for row in range(self.row):
+                    for col in range(self.row):
+                        if self.map[row][col].groupCellsIndex == valueDel:
+                            self.map[row][col].groupCellsIndex = self.map[cell_1[0]][cell_1[1]].groupCellsIndex 
+        for x in range(100):
+            if len(self.breakabel_whall) <= 100:
+                break
+            whall = self.findDestructibleWhall()
+            self.map[whall[0]][whall[1]].whall = False
+        self.map[1][0].whall = False
+        self.map[self.row-2][self.row-1].whall = False
+        self.map[self.row-2][self.row-1].distAtStart = 0
+
+    def generateMaze(self):
+        self.map = [[cell() for col in range(self.col)] for row in range(self.row)]
+        size = self.row
+        if size % 2 == 0:
+            size  += 1
+        i = 0
+        for row in range(size):
+            for col in range(size):
+                is_whall = (row % 2 == 0) or (col % 2 == 0) or row == 0 or row == size-1 or col == 0 or col == size-1
+                self.map[row][col].whall = is_whall
+                if ((row % 2 == 0) ^ (col % 2 == 0)) and row != 0 and row != size-1 and col != 0 and col != size-1:
+                    self.breakabel_whall.append([row, col])
+                if not is_whall:
+                    self.map[row][col].groupCellsIndex = i
+                    i += 1
+        self.makeComplexMaze()
+
+    def findWay(self):
+        futurMap = self.map
+        for row in range(len(self.map)):
+            for col in range(len(self.map)):
+                if not self.map[row][col].whall and self.map[row][col].distAtStart == -1:
+                    posToCheck = [[0,1],[0,-1],[1,0],[-1,0]]
+                    if row == 1 and col == 0:
+                        posToCheck = [[0,1]]
+                    for pos in posToCheck:
+                        if self.map[row + pos[0]][ col + pos[1]].distAtStart != -1:
+                            futurMap[row][col].distAtStart = self.map[row + pos[0]][ col + pos[1]].distAtStart + 1
+                            break
+        self.map = futurMap
 
 
 def makeCellsMap(rows, cols, cell_size):
@@ -130,20 +126,23 @@ def updateCellsMap(maze, cellsMap, size):
 def launch():
     cellsMap = makeCellsMap(rows, cols, cell_size)
     maze = Maze()
-    maze.generateMaze(rows)
+    maze.row = rows
+    maze.col = cols
+    maze.generateMaze()
     updateCellsMap(maze.map ,cellsMap, rows)
+    print(f"len map : {len(maze.map)}, len cell {len(cellsMap)}")
     while maze.map[1][0].distAtStart == -1:
         maze.findWay()
-        updateCellsMap(maze.maze ,cellsMap, rows)
+        updateCellsMap(maze.map ,cellsMap, rows)
 
 
 print(hsvToHex(0.91,1,1))
 root = Tk()
 root.title("Maze Resolver")
 root.resizable(width = False, height = False)
-cell_size = 15;
-rows = 51
-cols = 51
+cell_size = 3;
+rows = 201
+cols = 201
 canvas_width = cols * cell_size
 canvas_height = rows * cell_size
 canvas = Canvas(root, width = canvas_width, height = canvas_height)
